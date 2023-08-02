3d903a77c0608ca0f2a2cf31127278c56184b392
Alexandr <jwchaganav@gmail.com>
2023-08-02 13:10:22 +0400

    Revert "delete static constant" изменим название
    This reverts commit 59fd8543c6554f9df71c488f74f649ce2aed68ab.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,9 @@ import javax.crypto.spec.PSource;
 import java.sql.*;
 
 public class Main {
+//    private static final String USERNAME = "chagan";
+//    private static final String PASSWORD = "admin";
+//    private static final String URL = "jdbc:mysql://192.168.9.41:3306/mysql?userSSL=false";
     public static void main(String[] args) throws SQLException {
         System.out.println("Cистема учета для питомника домашних и вьючных животных");
         App app = new App();
