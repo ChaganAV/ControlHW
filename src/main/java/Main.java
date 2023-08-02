88102083b2498cd150d9d4f19f8d8c438ad7e213
Alexandr <jwchaganav@gmail.com>
2023-08-02 13:06:48 +0400

    правка наименования

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,8 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        System.out.println("Cистема учета для питомника");
+        System.out.println("Cистема учета для питомника домашних и вьючных животных");
         App app = new App();
         app.selectDogs();
-
-
     }
 }
